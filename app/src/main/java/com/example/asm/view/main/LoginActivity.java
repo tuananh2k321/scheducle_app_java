b15af831a25cee33d3be6f64384b3b252a3a90a3
AiCyZy <tranhet79@gmail.com>
2023-07-21 12:32:40 +0700

    ggLogin

---
+++ app/src/main/java/com/example/asm/view/main/LoginActivity.java
@@ -0,0 +1,143 @@
+package com.example.asm.view.main;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.asm.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    Button btnLogin;
+    String dropSpinner[] = {"FPT Polytechnic HO", "FPT Polytechnic Hà Nội", "FPT Polytechnic Hồ Chí Minh", "FPT Polytechnic Đà Nẵng",
+            "FPT Polytechnic Cần Thơ", "FPT Polytechnic Tây Nguyên", "FPT Polytechnic Hải Phòng", };
+
+    //gg auth
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    GoogleSignInClient gsc;
+    ProgressDialog progressDialog;
+
+    int RC_SIGN_IN = 40;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        spinner = findViewById(R.id.spinner);
+        btnLogin = findViewById(R.id.btn);
+
+        //spinner item
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, dropSpinner);
+        adapter.setDropDownViewResource(R.layout.custom_spinner);
+        spinner.setAdapter(adapter);
+
+        FirebaseApp.initializeApp(this);
+        //gg
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("Tạo account!!");
+        progressDialog.setMessage("Tạo thành công!!");
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+    }
+
+
+    private void signIn() {
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                firebaseAuth(account.getIdToken());
+            }catch (ApiException e){
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            Users users = new Users();
+                            users.setUserId(user.getUid());
+                            users.setName(user.getDisplayName());
+                            users.setProfile(user.getPhotoUrl().toString());
+
+                            database.getReference()
+                                    .child("Users")
+                                    .child(user.getUid())
+                                    .setValue(users);
+
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Đăng nhập không thành công!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
